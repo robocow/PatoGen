8efa20237414b2ef97627de528e23e935760690c
Mark G <mark@robocow.net>
2017-09-26 15:57:32 +0200

    Inital

---
+++ PatoGen/Main.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.cdPicker = New System.Windows.Forms.ColorDialog()
+        Me.tabMain = New System.Windows.Forms.TabControl()
+        Me.tabGradient = New System.Windows.Forms.TabPage()
+        Me.barEffect = New System.Windows.Forms.TrackBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtEffectChannel = New System.Windows.Forms.TextBox()
+        Me.txtSaveFileName = New System.Windows.Forms.TextBox()
+        Me.btnWrite = New System.Windows.Forms.Button()
+        Me.picGradientPreview = New System.Windows.Forms.PictureBox()
+        Me.picGradientColor2 = New System.Windows.Forms.PictureBox()
+        Me.picGradientColor1 = New System.Windows.Forms.PictureBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.dlgSceneFileSave = New System.Windows.Forms.SaveFileDialog()
+        Me.tabMain.SuspendLayout()
+        Me.tabGradient.SuspendLayout()
+        CType(Me.barEffect, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picGradientPreview, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picGradientColor2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picGradientColor1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tabMain
+        '
+        Me.tabMain.Controls.Add(Me.tabGradient)
+        Me.tabMain.Controls.Add(Me.TabPage2)
+        resources.ApplyResources(Me.tabMain, "tabMain")
+        Me.tabMain.Name = "tabMain"
+        Me.tabMain.SelectedIndex = 0
+        '
+        'tabGradient
+        '
+        Me.tabGradient.Controls.Add(Me.barEffect)
+        Me.tabGradient.Controls.Add(Me.Label1)
+        Me.tabGradient.Controls.Add(Me.txtEffectChannel)
+        Me.tabGradient.Controls.Add(Me.txtSaveFileName)
+        Me.tabGradient.Controls.Add(Me.btnWrite)
+        Me.tabGradient.Controls.Add(Me.picGradientPreview)
+        Me.tabGradient.Controls.Add(Me.picGradientColor2)
+        Me.tabGradient.Controls.Add(Me.picGradientColor1)
+        resources.ApplyResources(Me.tabGradient, "tabGradient")
+        Me.tabGradient.Name = "tabGradient"
+        Me.tabGradient.UseVisualStyleBackColor = True
+        '
+        'barEffect
+        '
+        resources.ApplyResources(Me.barEffect, "barEffect")
+        Me.barEffect.Maximum = 255
+        Me.barEffect.Name = "barEffect"
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'txtEffectChannel
+        '
+        resources.ApplyResources(Me.txtEffectChannel, "txtEffectChannel")
+        Me.txtEffectChannel.Name = "txtEffectChannel"
+        '
+        'txtSaveFileName
+        '
+        Me.txtSaveFileName.ForeColor = System.Drawing.SystemColors.GrayText
+        resources.ApplyResources(Me.txtSaveFileName, "txtSaveFileName")
+        Me.txtSaveFileName.Name = "txtSaveFileName"
+        '
+        'btnWrite
+        '
+        resources.ApplyResources(Me.btnWrite, "btnWrite")
+        Me.btnWrite.Name = "btnWrite"
+        Me.btnWrite.UseVisualStyleBackColor = True
+        '
+        'picGradientPreview
+        '
+        resources.ApplyResources(Me.picGradientPreview, "picGradientPreview")
+        Me.picGradientPreview.Name = "picGradientPreview"
+        Me.picGradientPreview.TabStop = False
+        '
+        'picGradientColor2
+        '
+        Me.picGradientColor2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        resources.ApplyResources(Me.picGradientColor2, "picGradientColor2")
+        Me.picGradientColor2.Name = "picGradientColor2"
+        Me.picGradientColor2.TabStop = False
+        '
+        'picGradientColor1
+        '
+        Me.picGradientColor1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        resources.ApplyResources(Me.picGradientColor1, "picGradientColor1")
+        Me.picGradientColor1.Name = "picGradientColor1"
+        Me.picGradientColor1.TabStop = False
+        '
+        'TabPage2
+        '
+        resources.ApplyResources(Me.TabPage2, "TabPage2")
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'dlgSceneFileSave
+        '
+        Me.dlgSceneFileSave.CheckFileExists = True
+        Me.dlgSceneFileSave.CreatePrompt = True
+        resources.ApplyResources(Me.dlgSceneFileSave, "dlgSceneFileSave")
+        Me.dlgSceneFileSave.ValidateNames = False
+        '
+        'frmMain
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.Controls.Add(Me.tabMain)
+        Me.Name = "frmMain"
+        Me.tabMain.ResumeLayout(False)
+        Me.tabGradient.ResumeLayout(False)
+        Me.tabGradient.PerformLayout()
+        CType(Me.barEffect, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picGradientPreview, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picGradientColor2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picGradientColor1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents cdPicker As ColorDialog
+    Friend WithEvents tabMain As TabControl
+    Friend WithEvents tabGradient As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents picGradientPreview As PictureBox
+    Friend WithEvents picGradientColor2 As PictureBox
+    Friend WithEvents picGradientColor1 As PictureBox
+    Friend WithEvents btnWrite As Button
+    Friend WithEvents dlgSceneFileSave As SaveFileDialog
+    Friend WithEvents txtSaveFileName As TextBox
+    Friend WithEvents barEffect As TrackBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtEffectChannel As TextBox
+End Class
---
+++ PatoGen/Main.vb
@@ -0,0 +1,250 @@
+﻿Public Class frmMain
+
+    Public MAXPIXELS = 50
+    Public FIXTURESIZE = 7
+    Public RGBFIXTUREOFFSET = 3
+    Public MAXFRAMES = 12000                              ' five minutes at 40 frames per second
+
+    Dim output(MAXFRAMES, MAXPIXELS * FIXTURESIZE) As Short       ' output(frame number, channel)
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Private Sub picGradientColor1_Click(sender As Object, e As EventArgs) Handles picGradientColor1.Click
+        If cdPicker.ShowDialog() = DialogResult.OK Then
+            Me.picGradientColor1.BackColor = cdPicker.Color
+            GradientDisplay()
+        End If
+
+    End Sub
+
+    Private Sub picGradientColor2_Click(sender As Object, e As EventArgs) Handles picGradientColor2.Click
+
+        If cdPicker.ShowDialog() = DialogResult.OK Then
+            Me.picGradientColor2.BackColor = cdPicker.Color
+            GradientDisplay()
+        End If
+
+    End Sub
+
+    Public Sub GradientDisplay()
+
+        'gradient display in control's paint handler
+        Me.Refresh()
+
+    End Sub
+
+
+    Private Sub picGradientPreview_Paint(sender As Object, e As PaintEventArgs) Handles picGradientPreview.Paint
+
+        Dim colorA As Color
+        Dim colorB As Color
+
+        colorA = Me.picGradientColor1.BackColor
+        colorB = Me.picGradientColor2.BackColor
+        Dim steps = Me.picGradientPreview.Width
+
+
+
+        ' colorB - colorA, care with negative
+
+        Dim redColorDiff, greenColorDiff, blueColorDiff As Integer
+        redColorDiff = Int(colorB.R) - Int(colorA.R)
+        greenColorDiff = Int(colorB.G) - Int(colorA.G)
+        blueColorDiff = Int(colorB.B) - Int(colorA.B)
+
+        Dim redres, grnres, blures As Integer
+        Dim colres As Color
+
+
+        For x = 0 To steps
+
+            redres = Int((redColorDiff / steps) * x) + Int(colorA.R)
+            grnres = Int((greenColorDiff / steps) * x) + Int(colorA.G)
+            blures = Int((blueColorDiff / steps) * x) + Int(colorA.B)
+
+            colres = Color.FromArgb(255, redres, grnres, blures)
+
+            'draw line 
+            Dim pen As New Pen(colres, 1)
+
+            'vertical lines
+            Dim y1 As Integer = 0
+            Dim y2 As Integer = Me.picGradientPreview.Height
+
+            e.Graphics.DrawLine(pen, x, y1, x, y2)
+
+        Next
+
+        renderGradientFrames(colorA, colorB, 800, 1)
+
+
+    End Sub
+
+    Private Sub renderGradientFrames(colorA As Color, colorB As Color, steps As Integer, phase As Short)
+
+        If steps < MAXFRAMES Then
+            'default is to cycle....
+        End If
+
+        Dim redColorDiff, greenColorDiff, blueColorDiff As Integer
+        redColorDiff = Int(colorB.R) - Int(colorA.R)
+        greenColorDiff = Int(colorB.G) - Int(colorA.G)
+        blueColorDiff = Int(colorB.B) - Int(colorA.B)
+
+
+        Dim redres, grnres, blures As Integer
+        Dim fixtureindex As Integer
+        Dim phased As Single
+
+        Dim x, y, z As Integer
+
+        For y = 0 To MAXPIXELS
+            fixtureindex = 0
+            For x = 0 To ((MAXPIXELS - 1) * FIXTURESIZE) Step FIXTURESIZE
+
+                phased = (fixtureindex + y) Mod MAXPIXELS
+
+                redres = Int((redColorDiff / MAXPIXELS) * phased) + Int(colorA.R)
+                grnres = Int((greenColorDiff / MAXPIXELS) * phased) + Int(colorA.G)
+                blures = Int((blueColorDiff / MAXPIXELS) * phased) + Int(colorA.B)
+
+                output(y, x + RGBFIXTUREOFFSET) = redres
+                output(y, x + 1 + RGBFIXTUREOFFSET) = grnres
+                output(y, x + 2 + RGBFIXTUREOFFSET) = blures
+
+                output(y, x + 6) = Int(txtEffectChannel.Text)
+
+                fixtureindex += 1
+            Next
+            z = y
+        Next
+
+
+        y = z
+
+        If True Then
+            Do Until z = 0
+
+                fixtureindex = 0
+                For x = 0 To ((MAXPIXELS - 1) * FIXTURESIZE) Step FIXTURESIZE
+
+                    phased = (fixtureindex + y) Mod MAXPIXELS
+
+                    redres = Int((redColorDiff / MAXPIXELS) * phased) + Int(colorA.R)
+                    grnres = Int((greenColorDiff / MAXPIXELS) * phased) + Int(colorA.G)
+                    blures = Int((blueColorDiff / MAXPIXELS) * phased) + Int(colorA.B)
+
+                    output(y, x + RGBFIXTUREOFFSET) = redres
+                    output(y, x + 1 + RGBFIXTUREOFFSET) = grnres
+                    output(y, x + 2 + RGBFIXTUREOFFSET) = blures
+
+                    output(y, x + 6) = Int(txtEffectChannel.Text)
+
+                    fixtureindex += 1
+
+                    y += 1
+                    z -= 1
+
+                Next
+
+            Loop
+        End If
+
+        'output stop
+        output(y, 0) = -10
+
+    End Sub
+
+    Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
+
+        'Dim saveFilename As String
+
+        'open/create file
+        Try
+            If dlgSceneFileSave.ShowDialog() = DialogResult.OK Then
+
+                txtSaveFileName.Text = dlgSceneFileSave().FileName
+
+            Else
+                Exit Sub
+            End If
+
+            Dim objWriter As IO.StreamWriter = IO.File.CreateText(txtSaveFileName.Text)
+            Dim x As Integer
+            x = 0
+
+            'do while output is not stop or ended
+            Do While output(x, 0) >= 0
+
+
+
+
+                If x > MAXFRAMES Then
+                    Throw New ApplicationException("Exceeded max frames")
+                End If
+
+
+                For y = 0 To MAXPIXELS * FIXTURESIZE
+
+                    If y < MAXPIXELS * FIXTURESIZE Then
+                        objWriter.Write(Str(output(x, y)) + ",")
+                    Else
+                        objWriter.Write(Str(output(x, y)))
+                    End If
+
+                Next
+
+                objWriter.Write(Environment.NewLine)
+                x += 1
+
+            Loop
+
+            'Dim bounceenabled = True
+
+            'If bounceenabled = True Then
+            '    Dim x2 As Integer
+            '    x2 = x
+            '    Do While x >= 0
+
+
+            '        For y = 0 To MAXPIXELS * FIXTURESIZE
+            '            If y < MAXPIXELS * FIXTURESIZE Then
+            '                objWriter.Write(Str(output(x2, y)) + ",")
+            '            Else
+            '                objWriter.Write(Str(output(x2, y)))
+            '            End If
+
+            '        Next
+
+            '        objWriter.Write(Environment.NewLine)
+            '        x -= 1
+            '        x2 += 1
+
+            '    Loop
+
+            'End If
+
+
+
+            objWriter.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+
+
+
+    End Sub
+
+    Private Sub barEffect_Scroll(sender As Object, e As EventArgs) Handles barEffect.Scroll
+        txtEffectChannel.Text = barEffect.Value
+
+    End Sub
+
+    Private Sub tabGradient_Click(sender As Object, e As EventArgs) Handles tabGradient.Click
+
+    End Sub
+
+End Class
